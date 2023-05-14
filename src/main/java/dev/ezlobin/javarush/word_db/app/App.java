dadf9fa2f106c6d744db120acb5d0076b2e84bb4
Zlobin Yevhen <zlobinyevhen0707@gmail.com>
2023-05-15 00:30:28 +0200

    First commit

---
+++ src/main/java/dev/ezlobin/javarush/word_db/app/App.java
@@ -0,0 +1,7 @@
+package dev.ezlobin.javarush.word_db.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/dev/ezlobin/javarush/word_db/servlet/HelloWorldServlet.java
@@ -0,0 +1,4 @@
+package dev.ezlobin.javarush.word_db.servlet;
+
+public class HelloWorldServlet {
+}
